628160afec3575561fb8441b56cf975fd8481089
Daniel Underwood <daniel.underwood13@gmail.com>
2014-09-21 16:48:40 -0400

    Remove extra paremeter in fitted phase function. Remove transpose of vectors and make the beta0s column vectors

--- afmAnalysisFromFile.m
+++ afmAnalysisFromFile.m
@@ -5,11 +5,11 @@ data = dlmread(filename, '\t');
 
 % Assign vectors from data
 % Frequency <=> x
-frequency = transpose(data(:,1));
+frequency = data(:,1);
 % Amplitude <=> y
-amplitude = transpose(data(:,2));
+amplitude = data(:,2);
 % Phase <=> phi
-phase = transpose(data(:,3));
+phase = data(:,3);
 
 % Declare amplitude fit model (Lorentzian)
 lorentzianModel = @(b,x) (b(1) + b(2)./((x - b(3)).^2 + b(4)));
@@ -17,8 +17,8 @@ lorentzianModel = @(b,x) (b(1) + b(2)./((x - b(3)).^2 + b(4)));
 phaseModel = @(b,x) (atan((((x .* b(1))./(b(2)))./(b(1).^2 - x.^2))));
 
 % Declare initial fit data
-beta0Amp = [min(amplitude) 10 300000 10];
-beta0Phase = [30000 450];
+beta0Amp = [min(amplitude); 10; 300000; 10];
+beta0Phase = [30000; 450];
 
 % Set Options for Fitting
 % Increase max iterations for better fit
@@ -34,7 +34,7 @@ fittedAmp = @(x) (betaAmp(1) + betaAmp(2)./((x - betaAmp(3)).^2 + betaAmp(4)));
 % Generate fit for frequency/phase
 betaPhase = nlinfit(frequency, phase, phaseModel, beta0Phase, options);
 % Make fitted function from regression coefficients
-fittedPhase = @(b,x) (atan((((x .* betaPhase(1))./(betaPhase(2)))./(betaPhase(1).^2 - x.^2))));
+fittedPhase = @(x) (atan((((x .* betaPhase(1))./(betaPhase(2)))./(betaPhase(1).^2 - x.^2))));
 
 % Generate amplitude plot
 figure
